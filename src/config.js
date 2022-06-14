63e8eea669cc2c90c47d1a90d886e37d85df3c7e
Burak0651 <103254618+BURAK06060606@users.noreply.github.com>
2022-06-14 23:04:17 +0300

    chat guard
    chat guard

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const config = { 
+//------------Connections-----------//
+    Client_Token: 'OTg2MzM0ODExNDUzMDIyMjM4.Ge63TV.Iyp1QyjvMdrqSZDN6PBHN3n1OAEWDdqLj9EFt4',
+    MongoDB_ConnectURL: 'mongodb+srv://burak:ba0651@chatguard.i68c3.mongodb.net/data?retryWrites=true&w=majority',
+    //------------BotSettings-----------//
+    Prefix: '!',
+    BotOwners: ["807188813616775168"],
+    Custom_Status_Text: 'Aroura',
+    Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
+    Custom_Status: 'online', // => dnd / idle / online / invisible
+    VoiceChannelID: '984130983651577856'
+};
+  
+module.exports = config;
